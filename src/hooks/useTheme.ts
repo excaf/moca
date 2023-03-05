5ca2e9804bc8f6b650d5078d14149dc87a567be6
excaf <yusufsamudera23@gmail.com>
2023-03-05 11:38:32 +0700

    fix: auto restore theme on page reload

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,11 +1,17 @@
 function toggleTheme() {
-    const themeSequences = new Map([
-        ["light", "dark"],
-        ["dark", "light"]
-    ])
-    const currTheme:string | undefined = document.documentElement.dataset.theme
-    
-    document.documentElement.dataset.theme = currTheme ? themeSequences.get(currTheme) : "dark"
+    let nextTheme: string;
+
+    switch (document.documentElement.dataset.theme) {
+        case "light":
+            nextTheme = "dark"; break;
+        case "dark":
+            nextTheme = "light"; break;
+        default:
+            nextTheme = "dark";
+    }
+
+    document.documentElement.dataset.theme = nextTheme
+    window.localStorage.setItem("theme", nextTheme)
 }
 
 export default function useTheme() {
